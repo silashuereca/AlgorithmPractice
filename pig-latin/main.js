032f635bd5b8e149aad142e3f728450ef27ab26d
silashuereca <silasgranthuereca@gmail.com>
2020-01-08 17:00:36 -0600

    finished pig-latin algorithm

--- pig-latin/main.js
+++ pig-latin/main.js
@@ -10,8 +10,25 @@
 // Input strings are guaranteed to be English words in all lowercase.
 
 function translatePigLatin(str) {
-    return str;
+  let regex = /[aeiou]/ig;
+  let notVowel = /^[^aeiou]+/ig
+  
+
+  // if first letter is a vowel then add "way" at the end of the string
+  if (regex.test(str[0])) {
+      return str + "way";
   }
+  // if the string doesn't contain any vowels add "ay" at the end  
+  if (regex.test(str) === false) {
+      return str + "ay";
+  }
+   // if the string contains vowels and the first letter isn't a vowel, return the collection
+   // of cononants plus "ay" at the end of the string
+  let firstConsonants = str.match(notVowel).join("");
+    
+  return str.replace(firstConsonants, "") + firstConsonants + "ay";
+}
+  
   
 console.log(translatePigLatin("consonant"));
 // output: "aliforniacay"
