1645d18ecc6c754ef4b43de01a6bf90efee68dfb
silashuereca <silasgranthuereca@gmail.com>
2020-01-20 17:08:57 -0600

    created algorithm structure

---
+++ convert-html-entities/main.js
@@ -0,0 +1,27 @@
+function convertHTML(str) {
+    // &colon;&rpar;
+    return str;
+  }
+  
+console.log(convertHTML("Dolce & Gabbana"));
+// output:
+
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+// output: 
+
+console.log(convertHTML("Sixty > twelve"));
+// output: 
+
+console.log(convertHTML('Stuff in "quotation marks"'));
+// output: 
+
+console.log(convertHTML("Schindler's List"));
+// output: 
+
+console.log(convertHTML("<>"));
+// output: 
+
+console.log(convertHTML("abc"));
+  // output: 
+
+  
