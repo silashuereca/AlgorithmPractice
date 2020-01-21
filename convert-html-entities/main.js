c9268a81c11a43283df47cd98986690f8917d0bc
silashuereca <silasgranthuereca@gmail.com>
2020-01-20 19:28:31 -0600

    solved html entities algorithm

--- convert-html-entities/main.js
+++ convert-html-entities/main.js
@@ -1,27 +1,46 @@
 function convertHTML(str) {
-    // &colon;&rpar;
-    return str;
+    let array = str.split("");
+    for (let i = 0; i < array.length; i++){
+        switch (array[i]) {
+          case '<':
+            array[i] = '&lt;'
+            break
+          case '>':
+            array[i] = '&gt;'
+            break
+          case '&':
+            array[i] = '&amp;'
+            break
+          case  '"':
+            array[i] = "&quot;"
+            break;
+          case "'":
+            array[i] = "&apos;"
+            break   
+        }
+    }
+    return array = array.join("");
   }
   
 console.log(convertHTML("Dolce & Gabbana"));
-// output:
+// output: Dolce &amp; Gabbana
 
 console.log(convertHTML("Hamburgers < Pizza < Tacos"));
-// output: 
+// output: Hamburgers &lt; Pizza &lt; Tacos
 
 console.log(convertHTML("Sixty > twelve"));
-// output: 
+// output: Sixty &gt; twelve
 
 console.log(convertHTML('Stuff in "quotation marks"'));
-// output: 
+// output: Stuff in &quot;quotation marks&quot;
 
 console.log(convertHTML("Schindler's List"));
-// output: 
+// output: Schindler&apos;s List
 
 console.log(convertHTML("<>"));
-// output: 
+// output: &lt;&gt;
 
 console.log(convertHTML("abc"));
-  // output: 
+  // output: abc
 
   
