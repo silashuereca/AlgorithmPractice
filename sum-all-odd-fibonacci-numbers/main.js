d186c0c91fcf8f84eb3ea7aa52faf5c3f8828c8d
silashuereca <silasgranthuereca@gmail.com>
2020-01-25 15:59:47 -0600

    solved sum all odd fibonacci numbers algorithm

--- sum-all-odd-fibonacci-numbers/main.js
+++ sum-all-odd-fibonacci-numbers/main.js
@@ -7,11 +7,28 @@ Every additional number in the sequence is the sum of the two previous numbers.
 The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
 
 For example sumFibs(10) should return 10 because
- all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, 5.
+all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, 5.
+
 */
 
 function sumFibs(num) {
-    return num;
+    if (num === 1) return 2;
+
+    let array = [1, 1];
+    let newNum = 0;
+    let total = []
+
+    for (let i = newNum; newNum <= num; i++){
+        if (newNum % 2 !== 0) {
+            total.push(newNum)    
+        }
+        newNum = array[0] + array[1];
+        array.shift();
+        array.push(newNum);
+            
+    }
+
+    return total.reduce(function(a, b){ return a + b}, 0) + 2;
   }
   
 console.log(sumFibs(4));
