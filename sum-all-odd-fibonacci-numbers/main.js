eff57b2db53f7cf444511542799e7768ec26d430
silashuereca <silasgranthuereca@gmail.com>
2020-01-25 14:39:27 -0600

    setup algorithm structure

---
+++ sum-all-odd-fibonacci-numbers/main.js
@@ -0,0 +1,27 @@
+/*
+Given a position integer num, 
+return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1.
+Every additional number in the sequence is the sum of the two previous numbers.
+The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example sumFibs(10) should return 10 because
+ all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, 5.
+*/
+
+function sumFibs(num) {
+    return num;
+  }
+  
+console.log(sumFibs(4));
+// output: 5
+
+console.log(sumFibs(1000));
+// output: 1785
+
+console.log(sumFibs(4000000));
+// output: 4613732
+
+console.log(sumFibs(75024));
+// output: 60696
