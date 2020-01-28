8ef14cd242cdcad6f744b14e81aa2a9a6984a3ab
silashuereca <silasgranthuereca@gmail.com>
2020-01-27 23:19:56 -0600

    added in sum all primes algorithm structure

---
+++ sum-all-primes/main.js
@@ -0,0 +1,24 @@
+/*
+A prime number is a whole number greater than 1 with exactly two divisors:
+1 and itself. 
+
+Rewrite sumPrimes so it returns the sum of all
+prime numbers that are less than or equal to num.
+*/
+
+function sumPrimes(num) {
+    let primeArray = [];
+    
+
+    for (let i = 2; i <= num; i++){
+       
+    }
+
+    return primeArray;
+}
+
+console.log(sumPrimes(10));
+// output: 17
+
+console.log(sumPrimes(977));
+// output: 73156
